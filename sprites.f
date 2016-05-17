3bf6eadf42bd4c2b114d9c102be0a12bdc5a02fd
rogerlevy <rogerlevy@localhost>
2016-05-17 14:57:44 -0400

    add flipFlags variable

--- sprites.f
+++ sprites.f
@@ -5,11 +5,16 @@ fixed
 : spriteset  ( w h string count -- <name> )  ( -- id )
   spritesets entry >r  <zfilespec> al_load_bitmap r@ initImage  r> subdivide ;
 
-: drawSpriteFlip  ( spriteset# subimage# flip -- )
-  >r  swap spritesets id> afsubimg  at@ 2af  r>  al_draw_bitmap_region ;
+: sprite>af  ( subimage# spriteset# -- afsx afsy afsw afsh )  spritesets id> afsubimg ;
 
-: drawSprite  ( spriteset# subimage# -- )
-  0 drawSpriteFlip ;
+variable flipFlags  \ the idea is that this will help in writing your own blit functions
+
+\ Quick and easy standard functions. anything more complicated than this isn't helpful.
+: drawSpriteFlip  ( subimage# spriteset# flip -- )
+  flipFlags !  sprite>af  at@ 2af  flipFlags @  al_draw_bitmap_region ;
+
+: drawSprite  ( subimage# spriteset# -- )  0 drawSpriteFlip ;
+
 
 : #sprites  ( spriteset# -- n )
   spritesets id> numSubimages @ ;
