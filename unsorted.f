bce312c022fcc4190eef2914c010b526edc100ca
rogerlevy <rogerlevy@localhost>
2016-05-17 21:31:16 -0400

    move /BOXES to unsorted.f

--- unsorted.f
+++ unsorted.f
@@ -1,29 +1,8 @@
-
-
-
-
-\ slow version ... not converted yet!
-
-\ : do-x
-\   vx @ -exit
-\     vx 2v@  vx @ u+  hitbox xy 2+  xy-ahb
-\     [[ .ahb >r ahb 4@ r@ 4@ overlap? if  r> ?lr  else  r> drop  then ]] boxes those
-\ ;
-\ : do-y
-\   vy @ -exit
-\     pos xy  vel y +   hitbox xy 2+  xy-ahb
-\     [[ .ahb >r ahb 4@ r@ 4@ overlap? if  r> ?tb  else  r> drop  then ]] boxes those
-\ ;
-\ %actor ~> %active
+\ generate random boxes
+\ : /boxes  ( -- )
+\   boxGrid resetCgrid
+\   #boxes 0 do  box one  ahb boxGrid addCbox  loop ;
 \
-\   0 0 16 16 hitbox box!
-\
-\   :: physics
-\     [ top# drop bottom# drop left# drop right# drop or or or ]# flags unset
-\     do-x
-\     do-y
-\     vel xy pos +xy
-\     pos xy hitbox xy 2+  xy-ahb
-\   ;
-\
-\ endp
+\ /boxes
+
+: *box  ( x y w h -- )  box one  w 2v! x 2v!  updateCbox  ahb boxGrid addCbox ;
