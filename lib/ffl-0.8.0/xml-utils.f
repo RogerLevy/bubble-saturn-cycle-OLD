29dabde6d462d4efbd13461d255d7aca5b712315
rogerlevy <rogerlevy@localhost>
2016-06-02 05:18:07 -0400

    add .NODE and SEARCH to xml-utils

--- lib/ffl-0.8.0/xml-utils.f
+++ lib/ffl-0.8.0/xml-utils.f
@@ -25,14 +25,47 @@ package doming
   : unnest  dom dom-parent 2drop ;
   : done  dom dom-(free) ;
   : name?  @name compare 0= ;
-  : sib?  ( addr c -- flag )  \ if true, current node is the found one.
+  : ?sib  ( addr c -- flag )  \ if true, current node is the found one.
     locals| c adr |
     dom dom-first  begin while  drop  adr c  name?  ?dup ?exit  ?next repeat
     false  ;
-  : attr?  nest sib? unnest ;
-  : @attrstr  ( addr c -- str count )  \ if not found, throw an error.
-    nest  sib? not if  unnest  xis.error throw  then  @str  unnest ;
+  : attr?  nest ?sib unnest ;
+  : @attr$  ( addr c -- str count )  \ if not found, throw an error.
+    nest  ?sib not if  -1 abort" Attribute not found."  then  @str  unnest ;
   : @attr  ( addr c -- n )  \ if not found, throw an error.
-    @attrstr evaluate ;
-  : .name  cr @name type ;
+    @attr$ evaluate ;
+  : type. ( n -- )
+    case
+      dom.attribute of ." (attribute)" endof
+      dom.element   of ." (element)" endof
+      dom.text      of ." (text)" endof
+      dom.cdata     of ." [CDATA]" endof
+      dom.comment   of ." (comment)" endof
+    endcase  space ;
+  : .name  cr @name type space  dom dom-get-type type. ;
+  : (.child)  @name type dom.attribute = if  ." : " @str type then space ;
+  : .node  .name  ."  Length: " dom dom-children . ."  Children: " ['] (.child) drill ;
+
+  : search  ( addr c xt -- flag )  ( ... dom-type -- found? ... )  \ if found, don't unnest
+    dom dom-children 0= if  2drop false exit  then
+    xt >r  to xt
+    dom dom-child
+      begin
+        .node
+        ( type ) xt execute
+        if  r> to xt  true exit
+        else  dom dom-next 0= if  unnest  r> to xt  false  exit then
+        then
+      again ;
+
+  :noname  ( addr c type -- addr c flag )
+    dom.element = dup -exit  drop  2dup name? ;
+    : ?elem  ( addr c -- false | true+nest )
+      [ literal ] search nip nip ;
+
+
+
+\  : @text  ( -- addr c )  \ will cause errors if element has no text.
+\    [ literal ] search drop unnest ;
+
 end-package
