ce65cfc78a3136bd8e017610c6407fc9ab720b9b
rogerlevy <rogerlevy@localhost>
2016-06-10 15:29:10 -0400

    add auto-pause feature.  when window loses focus, CPU load is 0%.

--- engine/piston.f
+++ engine/piston.f
@@ -5,8 +5,11 @@
 cr .( Press ALT-TILDE to toggle hitboxes etc. )
 : ?info  <tilde> kpressed alt? and if  info @ not info !  then ;
 : tick  poll  ?info  ?break  ?refresh  sim  lag ++ ;
+: switch-event
+  etype ALLEGRO_EVENT_DISPLAY_SWITCH_OUT = if  -timer  then
+  etype ALLEGRO_EVENT_DISPLAY_SWITCH_IN = if  clearkb  +timer  then ;
 : close-event  etype ALLEGRO_EVENT_DISPLAY_CLOSE = if  0 ExitProcess  then ;
-: common-events  ( kbstate-events )  close-event ;
+: common-events  ( kbstate-events )  close-event switch-event ;
 : timer-event  etype ALLEGRO_EVENT_TIMER = if  tick  then ;
 : game-events  common-events  timer-event ;
 : need-update?  eventq al_is_event_queue_empty  lag @ 4 >=  or ;                ( -- flag )
