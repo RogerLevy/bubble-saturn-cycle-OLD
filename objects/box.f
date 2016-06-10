df336a43314069dd08fa000bdae65e04084c600e
rogerlevy <rogerlevy@localhost>
2016-06-10 15:29:38 -0400

    remove stack traces from ?box and onLoadBox callback

--- objects/box.f
+++ objects/box.f
@@ -4,10 +4,10 @@ actor super class box
 
 : *box  ( w h -- )  box one  w 2v!  updateCbox  ahb boxGrid addCbox ;
 
-: ?box  over 0= over 0= or not if  .s  *box  else  2drop  then ;
+: ?box  over 0= over 0= or not if  *box  else  2drop  then ;
 
 cgridding +order
 :noname [ is onLoadBox ]  ( pen=xy -- )
-  ['] ?box  " width" @attr " height" @attr at@ 2+  sectw secth  .s stride2d ;
+  ['] ?box  " width" @attr " height" @attr at@ 2+  sectw secth  stride2d ;
 cgridding -order
 doming -order
