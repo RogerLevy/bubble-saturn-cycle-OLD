8ae2f2b69a10db79dbb307fad90cc8384e9e0d27
rogerlevy <rogerlevy@users.noreply.github.com>
2016-06-28 00:51:43 -0400

    test quicky bird head moaning sample (verdict: room for improvement)

--- map-tokens.f
+++ map-tokens.f
@@ -1,4 +1,4 @@
 
 \ tokens all have stack diagram ( -- )
 : player-drop  at@ player put ;
-: eagle-dialog   @dims talky zone>  trigger>  dialog> cr ." IT WORKS" ;
+: eagle-dialog   @dims talky zone>  trigger>  dialog>  *moan* ;
