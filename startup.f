6df9267ee4f192b7c415f9c2250f3997e742930e
rogerlevy <rogerlevy@localhost>
2016-06-30 20:02:30 -0400

    change how maps are loaded.  got rid of MAP, now it's LOAD

--- startup.f
+++ startup.f
@@ -1,11 +1,18 @@
 : cleanup  ( -- )  cleanup  boxGrid resetGrid  dynGrid resetGrid ;
 
-: map  ( tmxpath c area# -- <name> )  ( -- )  \ area# points to an entry in a table
-  create  , string,
-  does>  @+ to area#  count  cleanup  loadTMX ;
+\ this is wrong.  i put the bg gfx selection in the tmx file where it should be. 6/29/2016
+\ : map  ( tmxpath c area# -- <name> )  ( -- )  \ area# points to an entry in a table
+\   create  , string,
+\   does>  @+ to area#  count  cleanup  loadTMX ;
+\
+\
+\ " data\maps\W01_A02_v01.tmx" 0 map sfw0
+
+: findmap  " data/maps/" s[  bl parse +s  " .tmx" +s  ]s ;
+
+: load  ( -- <name> ) \ load a map.  no .tmx required
+    cleanup  findmap loadtmx ;
 
 
-" data\maps\W01_A02_v01.tmx" 0 map sfw0
-
-sfw0
+load solflood-west
 
