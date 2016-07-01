0caa5df9eedb2b9158118072c2a504ef17a73ed4
rogerlevy <rogerlevy@localhost>
2016-07-01 18:44:18 -0400

    factor trilobite PATROL

--- objects/trilobite.f
+++ objects/trilobite.f
@@ -1,8 +1,9 @@
 actor super class trilobite
 
-: patrol  40 80 rnd + 3 * perform>
-  begin 0.5 vx ! FLIP_H flip ! dup frames
-        -0.5 vx !     0 flip ! dup frames again ;
+: forth  -0.5 vx !    0 flip ! dup frames ;
+: back  0.5 vx ! FLIP_H flip ! dup frames ;
+
+: patrol  40 80 rnd + 3 * perform>  begin back forth again ;
 
 trilobite start:
   10 6 boxx 2v!  24 14 w 2v!  22 11 orgx 2v!
