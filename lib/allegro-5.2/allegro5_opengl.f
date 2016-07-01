3b534d17936253937ec59be50420ad687345ffec
rogerlevy <rogerlevy@localhost>
2016-06-30 20:00:15 -0400

    upgrade to Allegro 5.2

---
+++ lib/allegro-5.2/allegro5_opengl.f
@@ -0,0 +1,7 @@
+\ {nodoc}
+\ ==============================================================================
+\ ForestLib > Allegro5
+\ Allegro/OpenGL bindings
+\ ========================= copyright 2014 Roger Levy ==========================
+
+function: al_get_opengl_extension_list ( -- ALLEGRO_OGL_EXT_LIST )
