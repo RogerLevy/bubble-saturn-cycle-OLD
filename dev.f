4a660a73b7cfd09cdbc8d2b5ca33d12635e16c7e
rogerlevy <rogerlevy@users.noreply.github.com>
2016-07-09 01:44:24 -0400

    add error tolerance to (RENDER) and TICK (if dev if defined, or the IDE is loaded.)

--- dev.f
+++ dev.f
@@ -2,16 +2,14 @@ empty
 [undefined] dev [if] true constant dev gild [then]
 
 include toc
-\ include dev\gameprompt
 include dev\export
+include dev\ide.f
 include dev\doubledip
 
 \ some development helpers
 : s   sim ;  \ single-step!
-
 : :now  :noname ;  \ execute a nameless definition immediately
 : ;;  postpone ; execute ; immediate
 
-\ ok
 ide
 
--- engine/saturn/task-sf.f
+++ engine/saturn/task-sf.f
@@ -1,5 +1,14 @@
 \ Actor tasking system.  SwiftForth implementation.
 
+\ The following words should only be used within a task:
+\  YIELD END FRAMES SECS
+\ The following words should never be used within a task:
+\  - External calls
+\  - Console output (this might be able to be worked around)
+\  - EXIT or ; from the "root" (the definition containing PERFORM> )
+
+
+
 actor super
   var sp
   var rp
