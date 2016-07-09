7afbf5ddb9e046b043a96686ad5fa5923b902536
rogerlevy <roger.levy@gmail.com>
2016-07-09 18:50:25 -0400

    clean up / get ready to add engine subrepo

--- engine/saturn/load.f
+++
@@ -1,7 +0,0 @@
-: cleanup  ( -- )  cleanup  boxGrid resetGrid  dynGrid resetGrid ;
-
-: findmap  " data/maps/" s[  bl parse +s  " .tmx" +s  ]s ;
-
-: load  ( -- <name> ) \ load a map.  no .tmx required
-    cleanup  findmap loadtmx ;
-
