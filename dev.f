7056ea1388acce185006bf54966f3e618ab241fe
rogerlevy <rogerlevy@users.noreply.github.com>
2016-07-09 18:50:25 -0400

    clean up / get ready to add engine subrepo

--- dev.f
+++ dev.f
@@ -2,14 +2,15 @@ empty
 [undefined] dev [if] true constant dev gild [then]
 
 include toc.f
-\ include dev\export.f
-include dev\doubledip.f
+\ include engine\dev\export.f
+\ include engine\dev\gameprompt.f
+include engine\dev\doubledip.f
 
 \ some development helpers
 : s   sim ;  \ single-step!
 : :now  :noname ;  \ execute a nameless definition immediately
 : ;;  postpone ; execute ; immediate
 
-\ include dev\ide.f
+\ include engine\dev\ide.f
 \ ide
 ok
--- engine/modules/postproc.f
+++
@@ -1,9 +0,0 @@
-\ post-processing module
-
-\  internals:
-\    set up a target bitmap
-\    clear it
-\    execute rendering code, possibly with a transform applied
-\    render the target bitmap using some effect
-\    restore previous target bitmap
-
