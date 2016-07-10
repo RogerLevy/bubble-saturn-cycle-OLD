bfa0b04ac9a873c67a7216126c7848649e28d88b
rogerlevy <rogerlevy@users.noreply.github.com>
2016-07-09 21:35:46 -0400

    add remaining game specific files (still getting the hang of SourceTree...)

---
+++ obj/waveytest.f
@@ -0,0 +1,8 @@
+[undefined] draw-bitmap-wavey [if] include engine\modules\fx\wavey [then]
+
+actor super class waveytest
+
+: ?more  x 2v@ player 's x 2v@ proximity  10 /  10 min   10 swap - ;
+\ : ?more  1 ;
+
+waveytest start:    show>  testcard.image bmp @   0 0 32 32  x 2v@  #frames 10 * 50  ?more  draw-bitmap-wavey  ;
