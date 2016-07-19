1b074fd9dfa90a030d8edefa871e039b5bccf554
rogerlevy <rogerlevy@users.noreply.github.com>
2016-07-19 01:38:42 -0400

    bring up-to-date; add parabg object

--- obj/bgobj.f
+++ obj/bgobj.f
@@ -1,4 +1,4 @@
-doming +order
+xmling +order
 
 actor super
   var subtype  \ index into current bg object table (pointed to by tbl)
@@ -6,10 +6,11 @@ class bgobj
 
 /image allotment constant bgimg  \ maybe not the best way to do this but ... it works (i mean the whole one-image-for-everything bit)
 : img>  bgObjTable subtype @ th @ bgimg initImage bgimg ;
-: /subtype  ( -- )  " gid" @attr $fffffff and  me class @ firstgid @  -  subtype ! ;
+
+: /subtype  " gid" attr $fffffff and  me class @ firstgid @  -  subtype ! ;
 : ?autobox  " nobox" ?prop if drop exit then  x 2v@ at  w 2v@ *box ;
 
 bgobj start:  show>  img>  ?dup -exit  fitImage ;
 bgobj onMapLoad:  /dims  /subtype  /flip  ?autobox ;
 
-doming -order
+xmling -order
