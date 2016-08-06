583974a9727ff5ffa1eaea2038904a10bff1b5d9
rogerlevy <roger.levy@gmail.com>
2016-08-06 02:37:10 -0400

    refactor sc-specific stuff into the object pack.  rename toc to main.

--- dev.f
+++ dev.f
@@ -1,18 +1,18 @@
 empty
 [undefined] dev [if] true constant dev gild [then]
 
-include toc.f
-\ include engine\dev\export.f
-\ include engine\dev\gameprompt.f
+include main
 
-include engine\saturn\dev\doubledip.f
+\ include engine\dev\export
+\ include engine\dev\gameprompt
+
+\ include engine\saturn\dev\doubledip.f
 
 \ some development helpers
-: s   sim ;  \ single-step!
-: :now  :noname ;  \ execute a nameless definition immediately
-: ;;  postpone ; execute ; immediate
+\ : s   sim ;  \ single-step!
+\ : :now  :noname ;  \ execute a nameless definition immediately
+\ : ;;  postpone ; execute ; immediate
 
 
-include engine\dev\ide.f ide
-
+include engine\dev\ide  ide
 \ ok
